4154de0de62417f6f5d4bbc7a9b21d662a848423
Ralph Delia <Ralphdelia1@gmail.com>
2024-09-05 15:47:25 -0400

    D1 configured

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono';
+interface Bindings {
+	DB: D1Database;
+}
+const app = new Hono<{ Bindings: Bindings }>();
+app.get('/', async (c) => {
+	const stmt = c.env.DB.prepare('SELECT * FROM artworks LIMIT 3');
+	const { results } = await stmt.all();
 
-const app = new Hono();
-
-app.get('/', (c) => {
-	return c.json({ hello: 'world' });
+	return c.json(results);
 });
 
 export default app;
