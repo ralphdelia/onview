c2424943facbe1304ee2c63a78cc4423c3e71188
Ralph Delia <Ralphdelia1@gmail.com>
2024-10-09 07:26:58 -0400

    change container width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -66,7 +66,7 @@ const Layout = (props: { children?: any }) => {
 
           @media (min-width: 1280px) {
             .container {
-              max-width: 1280px;
+              max-width: 950px;
             }
           }
 
