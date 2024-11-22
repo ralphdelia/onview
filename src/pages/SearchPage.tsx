877530ebae5e9deb24612a3f72d77165d0bf7e2d
Ralph Delia <Ralphdelia1@gmail.com>
2024-11-22 13:15:48 -0500

    consolidiated scripts within the layout; removed script components

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,12 +1,10 @@
 import Layout from '../components/Layout';
 import NavBar from '../components/NavBar';
 import Search from '../components/Search';
-import SearchScript from '../components/SearchScript';
 
 const SearchPage = () => {
 	return (
 		<Layout>
-			<SearchScript />
 			<NavBar />
 			<Search />
 		</Layout>
