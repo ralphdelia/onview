e2d18c973769a91a607f7b35962f8685ad0c243b
Ralph Delia <Ralphdelia1@gmail.com>
2025-11-10 16:22:39 -0500

    added page search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,14 @@ export default defineConfig({
 	adapter: cloudflare({
 		imageService: 'compile',
 	}),
+	integrations: [
+		pagefind({
+			indexing: {
+				bundleDirectory: 'dist',
+				indexImages: true,
+			},
+		}),
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
