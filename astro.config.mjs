6dbcd584e3bf7ab77b22138756aebd9b6a1d6bb1
Ralph Delia <Ralphdelia1@gmail.com>
2025-11-11 14:16:33 -0500

    added astro sitemap, json-ld

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import pagefind from 'astro-pagefind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://onview.dev',
 	output: 'static',
 	adapter: cloudflare({
 		imageService: 'compile',
@@ -16,6 +18,7 @@ export default defineConfig({
 				indexImages: true,
 			},
 		}),
+		sitemap(),
 	],
 	vite: {
 		plugins: [tailwindcss()],
