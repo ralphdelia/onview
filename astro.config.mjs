7174a06e992cf7b0946a11ec8a7faffb56e6961b
Ralph Delia <Ralphdelia1@gmail.com>
2025-11-11 19:46:57 -0500

    update deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import pagefind from 'astro-pagefind';
 import sitemap from '@astrojs/sitemap';
@@ -8,9 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://onview.dev',
 	output: 'static',
-	adapter: cloudflare({
-		imageService: 'compile',
-	}),
 	integrations: [
 		pagefind({
 			indexing: {
